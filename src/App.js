3478f8b949e633e795e8bb9446d5d4bab5ff80e7
Alex Bailey <87785913+baileyam99@users.noreply.github.com>
2022-04-07 23:59:14 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {Adminadd, Adminedit} from './pages/Admin';
+import { AddCase, CaseList, IndiciesSearch, CaseMetrics } from './pages/Cases';
+import Edit from './pages/EditCredentials';
+
+function App() {
+  return (
+    <Router>
+      <Sidebar />
+      <Switch>
+        <Route path='/admin/add_user' exact component={Adminadd} />
+        <Route path='/admin/edit_user' exact component={Adminedit} />
+        <Route path='/cases/add_case' exact component={AddCase} />
+        <Route path='/cases/case_list' exact component={CaseList} />
+        <Route path='/cases/indicies_search' exact component={IndiciesSearch} />
+        <Route path='/cases/case_metrics' exact component={CaseMetrics} />
+        <Route path='/editcredentials/edit' exact component={Edit} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Cases.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export const AddCase = () => {
+  return (
+    <main>
+      <h1>Add Case</h1>
+      <h2>Sorry, this page is currently under construction.</h2>
+      <p>We'll finish it as quickly as we can. Thanks!</p>
+    </main>
+  );
+};
+
+export const CaseList = () => {
+  return (
+    <main>
+      <h1>Case List</h1>
+      <h2>Sorry, this page is currently under construction.</h2>
+      <p>We'll finish it as quickly as we can. Thanks!</p>
+    </main>
+  );
+};
+
+export const IndiciesSearch = () => {
+  return (
+    <main>
+      <h1>Indicies Search</h1>
+      <h2>Sorry, this page is currently under construction.</h2>
+      <p>We'll finish it as quickly as we can. Thanks!</p>
+    </main>
+  );
+};
+
+export const CaseMetrics = () => {
+  return (
+    <main>
+      <h1>Case Metrics</h1>
+      <h2>Sorry, this page is currently under construction.</h2>
+      <p>We'll finish it as quickly as we can. Thanks!</p>
+    </main>
+  );
+};
