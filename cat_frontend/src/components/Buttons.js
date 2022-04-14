4097f8e613e3da4333b0f5d1d836fb28b332ae90
Alex Bailey <87785913+baileyam99@users.noreply.github.com>
2022-04-14 14:26:54 -0400

    Add files via upload

---
+++ cat_frontend/src/components/Buttons.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const DetailsButton = styled.button`
+  background-color: #4290E8;
+  color: white;
+  font-size: 15px;
+  padding: 5px 20px;
+  border-radius: 5px;
+  margin: 10px 20px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #006de8;
+    font-weight: bold;
+    cursor: pointer
+  }
+`;
+
+export const FormSubmitButton = styled.button`
+  background-color: #4290E8;
+  color: white;
+  font-size: 15px;
+  padding: 5px 20px;
+  border-radius: 5px;
+  margin: 10px 20px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #006de8;
+    font-weight: bold;
+    cursor: pointer
+  }
+`;
