e9294494517371de655581e813e441f845b7a059
Alex Bailey <87785913+baileyam99@users.noreply.github.com>
2022-04-15 18:56:31 -0400

    Add files via upload

---
+++ cat_backend/CORS.php
@@ -0,0 +1,8 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials", "true");
+header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With");
+header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT");
+
+?>
