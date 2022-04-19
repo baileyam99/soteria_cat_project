8c2485be49a8aee5db6cf3797beddb4ec174fac1
Alex Bailey <87785913+baileyam99@users.noreply.github.com>
2022-04-18 20:25:37 -0400

    Login/Logout Button Styles

--- cat_frontend/src/components/login-button.js
+++ cat_frontend/src/components/login-button.js
@@ -1,15 +1,16 @@
+import { GeneralButton } from './Buttons';
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
   return (
-    <button
+    <GeneralButton
       className="btn btn-primary btn-block"
       onClick={() => loginWithRedirect()}
     >
       Log In
-    </button>
+    </GeneralButton>
   );
 };
 
--- cat_frontend/src/components/logout-button.js
+++ cat_frontend/src/components/logout-button.js
@@ -1,11 +1,11 @@
-
+import { GeneralButton } from './Buttons';
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
   return (
-    <button
+    <GeneralButton
       className="btn btn-danger btn-block"
       onClick={() =>
         logout({
@@ -14,7 +14,7 @@ const LogoutButton = () => {
       }
     >
       Log Out
-    </button>
+    </GeneralButton>
   );
 };
 
