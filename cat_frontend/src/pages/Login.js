b35ee1887a79028281903f8e894771ab74a54e71
Alex Bailey <87785913+baileyam99@users.noreply.github.com>
2022-04-19 14:52:15 -0400

    Add files via upload

---
+++ cat_frontend/src/pages/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AuthenticationButton from '../components/authentication-button';
+
+export const LoginPage = () => {
+    return (
+      <main>
+        <h1>Login</h1>
+        <AuthenticationButton />
+      </main>
+    );
+  };
